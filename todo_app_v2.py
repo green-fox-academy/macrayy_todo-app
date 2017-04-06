2a524f379135cec1a40ec79b0f24d6ac80d80073
Zsolt Makrai <mecrai@gmail.com>
2017-04-06 16:04:18 +0200

    refactor(todo): help prints

---
+++ todo_app_v2.py
@@ -0,0 +1,40 @@
+# TO DO APP
+import sys
+
+def argument_reader(arg):
+    if len(sys.argv) == 1:
+        help_text()
+    else:
+        if sys.argv[1] == "-l":
+            list_todo()
+
+def help_text():
+    return print("\n"
+            "Python Todo application\n"
+            "=======================\n"
+            "\n"
+            "Command line arguments:\n"
+            "-l   Lists all the tasks\n"
+            "-a   Adds a new task\n"
+            "-r   Removes a task\n"
+            "-c   Completes a task)\n")
+
+def read_file():
+    todo_file = open("todo.txt", "r")
+    todo_lines = todo_file.readlines()
+    todo_list = []
+    for line in todo_lines:
+        todo_list.append(line.split(";"))
+    todo_file.close()
+    return todo_list
+
+def list_todo():
+    read_file()
+    for element in todo_list:
+        if element[0] == "0":
+            print("[ ]" + " - " + todo_list[1])
+        elif element[0] == "1":
+            print("[x]" + " - " + todo_list[1])
+
+
+argument_reader(sys.argv)
