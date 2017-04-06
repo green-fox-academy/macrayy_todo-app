d14a21f58d5fb29672d901c2e63f4b3400bce1b0
Zsolt Makrai <mecrai@gmail.com>
2017-04-06 15:34:08 +0200

    ???

--- todo_app.py
+++ todo_app.py
@@ -1,33 +1,48 @@
 # TO DO APP
 import sys
-sys.argv
 
 class ArgumentReader():
     def argument_reader(self):
         print_help_text = View()
         if len(sys.argv) == 1:
-            return print_help_text.help_text()
+            print_help_text.help_text()
         else:
-            return sys.argv[1:]
+            if sys.argv[1] == "-l":
+                output_list = View()
+                output_list.list_todo()
+
+class OpenFile():
+
+    def read_file(self):
+        todo_file = open("todo.txt", "r")
+        todo_lines = todo_file.readlines()
+        todo_list = []
+        for line in todo_lines:
+            todo_list.append(line.split(";"))
+        todo_file.close()
+        return todo_list
 
 class View():
     def help_text(self):
-        return print("Python Todo application\n"
+        return print("\n"
+                "Python Todo application\n"
                 "=======================\n"
+                "\n"
                 "Command line arguments:\n"
                 "-l   Lists all the tasks\n"
                 "-a   Adds a new task\n"
-                "-r   Removes an task\n"
-                "-c   Completes an task)\n")
+                "-r   Removes a task\n"
+                "-c   Completes a task)\n")
+
+    def list_todo(self):
+        output_list = OpenFile()
+        output_list.read_file()
+        for element in output_list.read_file():
+            if output_list.read_file()[0] == "0":
+                print("[ ]" + " - " + output_list.read_file()[1])
+            elif output_list.read_file()[0] == "1":
+                print("[x]" + " - " + output_list.read_file()[1])
+
 
 arguments = ArgumentReader()
 arguments.argument_reader()
-
-
-# arguments = arg_reader()
-#
-# if len(arguments) == 0:
-# 	print('help text')
-# else:
-# 	if( arguments[0] == '-l' ):
-# 		print('Addolunk ilyet', arguments[1])
