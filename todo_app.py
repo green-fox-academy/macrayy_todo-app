1a45b970ffeff10ef419e9dc051f179a543db3bb
Zsolt Makrai <mecrai@gmail.com>
2017-04-06 13:58:10 +0200

    feat(todo): print help text

--- todo_app.py
+++ todo_app.py
@@ -1,1 +1,29 @@
 # TO DO APP
+import sys
+sys.argv
+
+
+def argument_reader():
+    if len(sys.argv) == 1:
+        return help_text()
+    else:
+        return sys.argv[1:]
+
+def help_text():
+    return print("Python Todo application\n"
+            "=======================\n"
+            "Command line arguments:\n"
+            "-l   Lists all the tasks\n"
+            "-a   Adds a new task\n"
+            "-r   Removes an task\n"
+            "-c   Completes an task)\n")
+
+arguments = argument_reader()
+
+# arguments = arg_reader()
+#
+# if len(arguments) == 0:
+# 	print('help text')
+# else:
+# 	if( arguments[0] == '-l' ):
+# 		print('Addolunk ilyet', arguments[1])
