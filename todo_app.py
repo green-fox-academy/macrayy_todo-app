18489d601d6ab7dc718629ff3ed40d01517b6abd
Zsolt Makrai <mecrai@gmail.com>
2017-04-06 14:08:50 +0200

    refactor(todo): prints the help

--- todo_app.py
+++ todo_app.py
@@ -2,23 +2,27 @@
 import sys
 sys.argv
 
+class ArgumentReader():
+    def argument_reader(self):
+        print_help_text = View()
+        if len(sys.argv) == 1:
+            return print_help_text.help_text()
+        else:
+            return sys.argv[1:]
 
-def argument_reader():
-    if len(sys.argv) == 1:
-        return help_text()
-    else:
-        return sys.argv[1:]
+class View():
+    def help_text(self):
+        return print("Python Todo application\n"
+                "=======================\n"
+                "Command line arguments:\n"
+                "-l   Lists all the tasks\n"
+                "-a   Adds a new task\n"
+                "-r   Removes an task\n"
+                "-c   Completes an task)\n")
 
-def help_text():
-    return print("Python Todo application\n"
-            "=======================\n"
-            "Command line arguments:\n"
-            "-l   Lists all the tasks\n"
-            "-a   Adds a new task\n"
-            "-r   Removes an task\n"
-            "-c   Completes an task)\n")
+arguments = ArgumentReader()
+arguments.argument_reader()
 
-arguments = argument_reader()
 
 # arguments = arg_reader()
 #
