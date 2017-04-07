0343b4c3df00b58e0ca3fc64c6e464d5ebfca9c4
Zsolt Makrai <mecrai@gmail.com>
2017-04-07 10:31:47 +0200

    refactor(todo): created controller class

--- todo_app_v2.py
+++ todo_app_v2.py
@@ -3,30 +3,22 @@ import sys
 import os
 os.system("cls" if os.name == "nt" else "clear")
 
-def argument_reader():
-    if len(sys.argv) == 1:
-        help_text()
-    else:
-        if sys.argv[1] == "-l":
-            list_todo()
-        elif sys.argv[1] == "-a":
-            if sys.argv[2:] == []:
-                print("Unable to add: no task provided.")
-            else:
-                add_task()
-                read_file()
+class Controller():
+    def argument_reader(self):
+        if len(sys.argv) == 1:
+            help_text()
+        else:
+            if sys.argv[1] == "-l":
                 list_todo()
+            elif sys.argv[1] == "-a":
+                if sys.argv[2:] == []:
+                    error_messages()
+                else:
+                    add_task()
+                    read_file()
+                    list_todo()
 
-def help_text():
-    return print("\n"
-            "Python Todo application\n"
-            "=======================\n"
-            "\n"
-            "Command line arguments:\n"
-            "-l   Lists all the tasks\n"
-            "-a   Adds a new task\n"
-            "-r   Removes a task\n"
-            "-c   Completes a task)\n")
+
 
 def read_file():
     todo_file = open("todo.txt", "r")
@@ -48,7 +40,7 @@ def add_task():
 def list_todo():
     read_file()
     if read_file() == []:
-        print("No todos for today! :)")
+        print("No todos for today! Good for you!")
     else:
         nth = 0
         for element in read_file():
@@ -58,5 +50,19 @@ def list_todo():
             elif element[0] == "1":
                 print(nth, "[*]", " - ", element[1])
 
+def help_text():
+    return print("\n"
+            "Python Todo application\n"
+            "=======================\n"
+            "\n"
+            "Command line arguments:\n"
+            "-l   Lists all the tasks\n"
+            "-a   Adds a new task\n"
+            "-r   Removes a task\n"
+            "-c   Completes a task)\n")
 
-argument_reader()
+def error_messages():
+    print("Unable to add: no task provided.")
+
+run = Controller()
+run.argument_reader()
